5258db3923d49d077e241a4b139afba2e82e721e
Kangkai Ye <yek@MacBook-Pro.local>
2021-03-14 22:38:02 +0100

    Readme

--- src/common/free.c
+++ src/common/free.c
@@ -6,21 +6,21 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 20:38:57 by kaye              #+#    #+#             */
-/*   Updated: 2021/03/14 19:19:25 by kaye             ###   ########.fr       */
+/*   Updated: 2021/03/14 22:37:33 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 
-void    clean_all(t_stack *stacks, char *line)
+void	clean_all(t_stack *stacks, char *line)
 {
-    if (line)
-        free(line);
-    if (stacks)
-    {
-        ft_lstclear(&stacks->a, NULL);
-        ft_lstclear(&stacks->b, NULL);
-    }
-    free(stacks);
-    stacks = NULL;
+	if (line)
+		free(line);
+	if (stacks)
+	{
+		ft_lstclear(&stacks->a, NULL);
+		ft_lstclear(&stacks->b, NULL);
+	}
+	free(stacks);
+	stacks = NULL;
 }
--- src/common/print.c
+++ src/common/print.c
@@ -6,26 +6,26 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 17:38:13 by kaye              #+#    #+#             */
-/*   Updated: 2021/03/14 22:35:56 by kaye             ###   ########.fr       */
+/*   Updated: 2021/03/14 22:37:00 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 
-void     print_stack(t_list *a, t_list *b)
+void	print_stack(t_list *a, t_list *b)
 {
-    if (a == NULL && b == NULL)
-        return ;
-    if (a && b)
-        ft_printf("%12d | %-12d\n", (int)a->content, (int)b->content);
-    else if (a)
-        ft_printf("%12d | %-12c\n", (int)a->content, ' ');
-    else if (b)
-        ft_printf("%12c | %-12d\n", ' ', (int)b->content);
-    if (a && b)
-        print_stack(a->next, b->next);
-    else if (a)
-        print_stack(a->next, NULL);
-    else if (b)
-        print_stack(NULL, b->next);
+	if (a == NULL && b == NULL)
+		return ;
+	if (a && b)
+		ft_printf("%12d | %-12d\n", (int)a->content, (int)b->content);
+	else if (a)
+		ft_printf("%12d | %-12c\n", (int)a->content, ' ');
+	else if (b)
+		ft_printf("%12c | %-12d\n", ' ', (int)b->content);
+	if (a && b)
+		print_stack(a->next, b->next);
+	else if (a)
+		print_stack(a->next, NULL);
+	else if (b)
+		print_stack(NULL, b->next);
 }
